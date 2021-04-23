88e0582bfa60739b1f4ccd347171b8361505cebf
Eldupalco <marcelodupalco@gmail.com>
2021-04-23 21:42:49 +0800

    yawa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('accountStudent.store') }}">
+                    <form method="POST" action="{{ route('register') }}">
                       @csrf
   
   
